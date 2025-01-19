351c55f7777fac9db0b740922fd271986cbc33ef
jeongjihyunn <1204jjh@naver.com>
2025-01-19 18:28:52 +0900

    sdk 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.studywithswu"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.studywithswu"

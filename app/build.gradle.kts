8b6030830ec0a0f9f12ef7baed0c620a057f558f
janghyeri <bluebird1477@naver.com>
2025-01-31 00:14:38 +0900

    뱃지 추가, 마이페이지 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     dependencies {
 
         // Firebase BOM 설정 추가
